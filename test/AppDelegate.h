1dcb1a03212aaf12119bb9619fc54d61f449b7bd
Ashish Thirunavalli <ashish2442@gmail.com>
2014-08-28 18:34:30 -0500

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Programer on 8/28/14.
+//  Copyright (c) 2014 icuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Programer on 8/28/14.
+//  Copyright (c) 2014 icuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
